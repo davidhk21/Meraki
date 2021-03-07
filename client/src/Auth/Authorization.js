8b51224e43976482902534a85e39a3aaa3d95b20
David Kim <davidhk21@gmail.com>
2021-03-06 23:42:07 -0700

    added Login page

--- client/src/Auth/Authorization.js
+++ client/src/Auth/Authorization.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import Welcome from './components/Welcome';
 import SignUp from './components/SignUp';
+import Login from './components/Login';
 
 const Authorization = () => {
   return (
@@ -12,10 +13,12 @@ const Authorization = () => {
           <ul>
             <li><a href="/">Welcome</a></li>
             <li><a href="/signup">Sign Up</a></li>
+            <li><a href="/login">Login</a></li>
           </ul>
         </nav>
         <Route path="/" exact component={Welcome} />
         <Route path="/signup" exact component={SignUp} />
+        <Route path="/login" exact component={Login} />
       </header>
     </div>
   );
