6c649d397afe5f2e9b5dbedcbf1fe6dd6290b228
David Kim <davidhk21@gmail.com>
2021-03-06 23:40:40 -0700

    rendered welcome and sign up with components

--- client/src/Auth/Authorization.js
+++ client/src/Auth/Authorization.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import Welcome from './components/Welcome';
+import SignUp from './components/SignUp';
+
 const Authorization = () => {
   return (
     <div>
       <header>
         <nav>
           <ul>
-            <li><a href="/">Home</a></li>
+            <li><a href="/">Welcome</a></li>
             <li><a href="/signup">Sign Up</a></li>
           </ul>
         </nav>
-        <Route path="/" exact render={() => <h1>Welcome</h1>} />
-        <Route path="/signup" exact render={() => <h1>Sign Up</h1>} />
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" exact component={SignUp} />
       </header>
     </div>
   );
